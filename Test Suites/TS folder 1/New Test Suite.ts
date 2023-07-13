6ee2437f709bcf2707c7de263d659b7127d7a6a3
lydoan1199 <lydoan@kms-technology.com>
2023-07-13 14:08:26 +0700

    add TS

---
+++ Test Suites/TS folder 1/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ca8031c8-947b-4682-b47f-404a7cc76f96</testSuiteGuid>
+</TestSuiteEntity>
